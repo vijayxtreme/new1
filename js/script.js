965a111762417d27b2d4bbfb958eb3c49761e76d
Equate <Equate@Equate-Vijay.local>
2016-05-03 15:19:16 -0700

    More changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-"use strict";
-$("#hamburger").on('click', function (e) {
-    alert('hamburger');
-});
+(function ($) {
+    //control for sidebar
+    (function sidebar() {
+        var sidebar = true;
+        $("#hamburger-desktop").one('click', showSideBar);
+        function showSideBar() {
+            $("aside.sidebar").show(600);
+            $("#hamburger-desktop").one('click', hideSideBar);
+            $(".container").addClass('sidebar-active');
+            sidebar = true;
+        }
+        function hideSideBar() {
+            $(".container").removeClass('sidebar-active');
+            $("aside.sidebar").hide();
+            $("#hamburger-desktop").one('click', showSideBar);
+            sidebar = false;
+        }
+        $("aside .close-btn").click(hideSideBar);
+        $(window).resize(function () {
+            if ($(this).width() < 992) {
+                if (sidebar) {
+                    hideSideBar();
+                }
+            }
+        });
+    })();
+})(jQuery);
--- ts/script.ts
+++ ts/script.ts
@@ -1,5 +1,33 @@
-"use strict"
+(function($){
+	//control for sidebar
+	(function sidebar(){
+		var sidebar = true;
 
-$("#hamburger").on('click', function(e){
-	alert('hamburger')
-})
+		$("#hamburger-desktop").one('click', showSideBar);
+
+		function showSideBar() {
+			$("aside.sidebar").show(600)
+			$("#hamburger-desktop").one('click', hideSideBar)
+			$(".container").addClass('sidebar-active')
+			sidebar = true;
+		}
+
+		function hideSideBar() {
+			$(".container").removeClass('sidebar-active')
+			$("aside.sidebar").hide()
+			$("#hamburger-desktop").one('click', showSideBar)
+			sidebar = false;
+		}
+
+		$("aside .close-btn").click(hideSideBar)
+
+		$(window).resize(function(){
+			if($(this).width() < 992){
+				if(sidebar){
+					hideSideBar();
+				}
+			}
+		})
+	})();
+
+})(jQuery);
