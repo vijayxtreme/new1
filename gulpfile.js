867952e7788a5599a691741c9e6d84574bfea266
Equate <Equate@Equate-Vijay.local>
2016-05-02 17:49:55 -0700

    Updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp'),
 	stylus = require('gulp-stylus'),
 	ts = require('gulp-typescript'),
+	cleanCSS = require('gulp-clean-css'),
 	livereload = require('gulp-livereload');
 
 gulp.task('styl', function () {
   return gulp.src('./styl/style.styl')
     .pipe(stylus())
     .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('css/*.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('css'));
 });
 
 gulp.task('ts', function(){
