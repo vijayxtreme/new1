cfaae77a0f3180142e4eaa6117fcfbc705600bd6
Equate <Equate@Equate-Vijay.local>
2016-05-06 17:26:03 -0700

    add navbar menu

--- js/script.js
+++ js/script.js
@@ -2,25 +2,51 @@
     //control for sidebar
     (function sidebar() {
         var sidebar = true;
-        $("#hamburger-desktop").one('click', showSideBar);
+        $("#hamburger-desktop, #hamburger-tablet").one('click', showSideBar);
         function showSideBar() {
             $("aside.sidebar").show(600);
-            $("#hamburger-desktop").one('click', hideSideBar);
-            $(".container").addClass('sidebar-active');
+            $("#hamburger-desktop, #hamburger-tablet").unbind().one('click', hideSideBar);
+            // $(".container").addClass('sidebar-active')
             sidebar = true;
         }
         function hideSideBar() {
-            $(".container").removeClass('sidebar-active');
-            $("aside.sidebar").hide();
-            $("#hamburger-desktop").one('click', showSideBar);
+            // $(".container").removeClass('sidebar-active')
+            $("aside.sidebar").hide(600);
+            $("#hamburger-desktop, #hamburger-tablet").unbind().one('click', showSideBar);
             sidebar = false;
         }
         $("aside .close-btn").click(hideSideBar);
         $(window).resize(function () {
-            if ($(this).width() < 992) {
+            if ($(this).width() < 600) {
                 if (sidebar) {
-                    hideSideBar();
+                    $("aside.sidebar").hide(0);
+                    $("#hamburger-desktop, #hamburger-tablet").unbind().one('click', showSideBar);
+                    sidebar = false;
                 }
+            }
+        });
+    })();
+    //control for search
+    (function search() {
+        $("#search").click(function () {
+            $("#searchbar").toggle();
+        });
+        $(window).resize(function () {
+            if ($(this).width() > 992) {
+                $("#searchbar").hide(0);
+            }
+        });
+    })();
+    //control for nav
+    (function menu() {
+        $(".review-menu ul li").click(function (e) {
+            $(this).children("ul").show(300).addClass("active");
+        });
+        $(document).mouseup(function (e) {
+            var container = $(".review-menu ul li ul");
+            if (!container.is(e.target) // if the target of the click isn't the container...
+                && container.has(e.target).length === 0) {
+                container.hide();
             }
         });
     })();
